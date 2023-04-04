a6b44d76c2a0fbc81c7ac45b0943ac91de4a9556
Sunny L <94069216+sunnylin933@users.noreply.github.com>
2023-04-04 14:17:31 -0400

    More Backgrounds

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -26,22 +26,49 @@ public class NPCController : MonoBehaviour
     {
         if (previousEnvironment != environmentID)
         {
+            source.Stop();
             switch (environmentID)
             {
-                case "forest":
-                    background.sprite = backgrounds[5];
-                    source.PlayOneShot(backgroundNoise[0]);
-                    break;
                 case "cabin":
                     background.sprite = backgrounds[0];
                     break;
                 case "large_city":
                     background.sprite = backgrounds[1];
+                    source.PlayOneShot(backgroundNoise[1]);
+                    source.volume = 0.1f;
                     break;
                 case "small_town":
                     background.sprite = backgrounds[2];
                     break;
-
+                case "tavern":
+                    background.sprite = backgrounds[3];
+                    break;
+                case "blacksmith":
+                    background.sprite = backgrounds[4];
+                    break;
+                case "forest":
+                    background.sprite = backgrounds[5];
+                    source.PlayOneShot(backgroundNoise[0]);
+                    source.volume = 0.25f;
+                    break;
+                case "lake":
+                    background.sprite = backgrounds[6];
+                    break;
+                case "meadow":
+                    background.sprite = backgrounds[7];
+                    break;
+                case "mountain":
+                    background.sprite = backgrounds[8];
+                    break;
+                case "river":
+                    background.sprite = backgrounds[9];
+                    break;
+                case "battlefield":
+                    background.sprite = backgrounds[10];
+                    break;
+                case "dungeon_prison":
+                    background.sprite = backgrounds[11];
+                    break;   
             }
 
             previousEnvironment = environmentID;
